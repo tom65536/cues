80be638a0e1b4ba327fae609635f08ebedf9a121
Thomas Reiter <tom65536@web.de>
2021-06-15 23:03:55 +0200

    initial project layout

---
+++ cues.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "Thomas Reiter"
+description   = "Puzzle Collection"
+license       = "GPL-3.0-or-later"
+srcDir        = "src"
+bin           = @["cues"]
+backend       = "js"
+
+# Dependencies
+
+requires "nim >= 1.4.8"
+requires "karax >= 1.2.1"
---
+++ src/cues.nim
@@ -0,0 +1,42 @@
+
+include karax/prelude
+
+type
+  Model = ref object
+    content : kstring
+
+  Message = enum
+    Change
+
+proc init() : Model =
+  Model(
+    content: ""
+  )
+
+proc update(model : Model, msg : Message) =
+  case msg
+    of Change:
+      model.content = "changed"
+
+proc view(model : Model) : VNode =
+  proc onChange(ev : Event, n : VNode) =
+    update(model, Message.Change)
+
+  buildHtml(tdiv):
+    h1(text "Hello Karax")
+
+    textarea(onkeyup = onChange)
+
+    tdiv:
+      span(text $model.content)
+
+proc main() =
+  var model = init()
+
+  proc render() : VNode =
+    view(model)
+
+  setRenderer render
+
+when isMainModule:
+  main()
